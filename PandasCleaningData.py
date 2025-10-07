f7b0dc7f6dfed34930b261ba398c617354b14b2d
KTokos <kristiantokos542@gmail.com>
2025-10-07 12:39:50 -0400

    Added a copy of the dataset, and "PandasCleaningData.py"

---
+++ PandasCleaningData.py
@@ -0,0 +1,25 @@
+# ---------- Data Cleaning ----------
+
+# Data cleaning means fixing bad data in your data set
+
+# Bad data could be:
+    # Empty cells
+    # Data in wrong format
+    # Wrong data
+    # Duplicates
+
+# In this tutorial you will learn how to deal with them
+
+# ---------- Our Data Set ----------
+
+# In the next chapters we will use this data set:
+
+# "originalData.csv", I recommend making a copy of it to work with and compare
+
+# The data set contains some empty cells ("Date" in row 22, and "Calories" in row 18 and 28)
+
+# The data set contains wrong format ("Date" in row 26)
+
+# The data set contains wrong data ("Duration" in row 7)
+
+# The data set contains duplicates (row 11 and 12)
