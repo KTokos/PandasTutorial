e6c33363f7629b8554c0aeac3f02a10f7b87ca05
KTokos <kristiantokos542@gmail.com>
2025-10-09 12:42:06 -0400

    Updated the CSV files to include a 'Date' column. Also created 'PandasCleaningWrongFormat.py' that uses the new CSV files.

---
+++ PandasCleaningWrongFormat.py
@@ -0,0 +1,43 @@
+# Imports
+import pandas as pd
+
+# Divider length
+dLen = '-' * 20
+
+# ---------- Data of Wrong Format ----------
+
+# Cells with data of wrong format can make it difficult, or even impossible, to analyze data
+
+# To fix it, you have two options: remove the rows, or convert all cells in the columns into the same format
+
+# ---------- Convert Into a Correct Format ----------
+
+# In our DataFrame, we have two cells with the wrong format. Check out row 21 and 25, the 'Date' column should be a
+# string that represents a date
+
+# Let's try to convert all cells in the 'Date' column into dates
+
+# Pandas has a "to_datetime()" method for this
+
+# Read CSV file into a DataFrame
+df = pd.read_csv('data.csv')
+
+# Convert to date
+df['Date'] = pd.to_datetime(df['Date'], format='mixed')
+
+# Print out the DataFrame
+print(df.to_string())   # Use "to_string()" to print the entire DataFrame
+
+# As you can see from the result, the date in row 25 was fixed, but the empty date in row 22 got a NaT (Not a Time)
+# value, in other words an empty value. One way to deal with empty values is simply removing the entire row
+
+# ---------- Removing Rows ----------
+
+# The result from the converting in the example above gave us a NaT value, which can be handled as a NULL value,
+# and we can remove the row by using the "dropna()" method
+
+# Remove rows with a NULL value in the "Date" column
+df.dropna(subset=['Date'], inplace=True)
+
+# Print out the entire DataFrame
+print(df.to_string())   # Use "to_string()" to print the entire DataFrame
